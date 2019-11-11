98d10b55552dfde1c329f6a0b9dd6b3c602c011d
Thinh Nguyen <thanhthinhcntt@gmail.com>
2019-11-11 11:12:00 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,50 @@
+console.log("Worked!");
+let yes = document.querySelector("#yes"),
+    no = document.querySelector("#no"),
+    yesChoice = document.querySelector(".yesChoice"),
+    noChoice = document.querySelector(".noChoice"),
+    exit = document.querySelectorAll(".fa-times-circle"),
+    button = document.querySelectorAll("button");
+let text = `Này cô gái dễ thương kia ơi, ừ, chính là cậu đó. < Tớ đã rất đắn đo khi viết những lời này. < Tớ không
+biết
+liệu  kết quả có được như mong muốn. < Nhưng cuối cùng tớ vấn muốn nói ra. < Vì đó là những thứ mà con
+tim tớ 
+mách bảo. < Từ lần đầu tiên gặp cậu, con tim tớ đã xốn xang. < Tớ chưa từng trải qua cảm giác đó trước
+đây. <
+Càng tiếp xúc với cậu, < tớ lại càng tin rằng, < cậu là người mà tớ muốn bảo vệ trong quãng đời còn lại. <
+Người mà tớ muốn mang lại hạnh phúc, muốn nhìn thấy nụ cười vui vẻ mãi mãi. |
+Vậy nên, tớ không biết rằng tớ có thể có được may mắn đó hay không. < Trở thành người thương của cậu,
+che
+chở cho cậu, chia sẻ gánh nặng cuộc đời với cậu. < Có được không ?`;
+let i = 0;
+let speed = 100;
+let para = document.querySelector("#txt");
+typeWriter();
+
+function typeWriter() {
+    if (i < text.length) {
+        if (text.charAt(i) == "<") para.innerHTML += "</br>";
+        else if (text.charAt(i) == "|") para.innerHTML = "";
+        else para.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed)
+    } else {
+        for (let j = 0; j < button.length; j++) {
+            button[j].style.visibility = "visible"
+        }
+    }
+}
+yes.addEventListener("click", function () {
+    noChoice.style.display = "none";
+    yesChoice.style.display = "flex"
+});
+no.addEventListener("click", function () {
+    noChoice.style.display = "block";
+    yesChoice.style.display = "none"
+});
+for (let i = 0; i < exit.length; i++) {
+    exit[i].addEventListener("click", function () {
+        noChoice.style.display = "none";
+        yesChoice.style.display = "none"
+    })
+}
---
+++ js/next.js
@@ -0,0 +1,3 @@
+(function () {
+    document.getElementById("text__photo").innerText = "Tớ luôn dõi theo cậu đấy, cậu biết không ?";
+})()
